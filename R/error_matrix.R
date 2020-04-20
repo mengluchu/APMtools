44ec30a14de69e87627fbf19e74b3cd486b199ca
Meng Lu <lumeng0312@gmail.com>
2020-04-20 20:53:28 +0200

    add relative matrix

--- R/error_matrix.R
+++ R/error_matrix.R
@@ -4,26 +4,45 @@
 #' @return an errormatrix with RMSE, MAE, IQR
 #' @export
 
+    error_matrix = function(validation, prediction) {
+        rmse <- function(test, pred) {
+            sqrt(mean((pred - test)^2))
+        }
+        MAE = function(test, pred) {
+            mean(abs(pred - test))
 
-error_matrix = function(validation, prediction) {
-    rmse <- function(test, pred) {
-        RMSE = sqrt(mean((pred - test)^2))
-        RMSE
-        # x= pred - test RMSE <- sqrt(sum(x^2) /length(x))
+        }
+        IQR <- function(test, pred) {
+            a2 = summary(as.vector(pred - test))
+            as.vector(a2[5] - a2[2])
+
+        }
+        rIQR <- function(test, pred) {
+            a2 = summary(as.vector(pred - test))
+            as.vector(a2[5] - a2[2])/median(test)
+            # divided by median
+        }
+        Rsquare <- function(test, pred) {
+            1-   var(pred - test)/var(test)
+        }
+
+        rrmse <- function(test, pred) {
+            rmse = sqrt(mean((pred - test)^2))
+            rmse/mean(test)
+        }
+        rMAE = function(test, pred) {
+            mean(abs(pred - test))/mean(test)
+        }
+        rmse1 = rmse(validation, prediction)
+        rrmse1 = rrmse(validation, prediction)
+
+        MAE1 = MAE(validation, prediction)
+        rMAE1 = rMAE(validation, prediction)
+        IQR1 = IQR(validation, prediction)
+        rIQR1 = rIQR(validation, prediction)
+
+        rsqd1 = Rsquare(validation, prediction)
+        c(RMSE = rmse1, RRMSE = rrmse1, IQR = IQR1, rIQR = rIQR1, MAE=MAE1, rMAE = rMAE1, rsq = rsqd1)
     }
-    MAE = function(test, pred) {
-        mae = mean(abs(pred - test))
-        mae
-    }
-    IQR <- function(test, pred) {
-        a2 = summary(as.vector(pred - test))
-        iqr_error = as.vector(a2[5] - a2[2])
-        iqr_error
-    }
-    rmse1 = rmse(validation, prediction)
-    
-    MAE1 = MAE(validation, prediction)
-    
-    IQR1 = IQR(validation, prediction)
-    c(RMSE = rmse1, MAE = MAE1, IQR = IQR1)
-}
+
+
