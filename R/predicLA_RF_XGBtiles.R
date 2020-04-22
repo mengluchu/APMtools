87dd825decdf527a0807f6c711a2111a9258447f
Meng Lu <lumeng0312@gmail.com>
2020-04-22 16:44:53 +0200

    fix data

--- R/predicLA_RF_XGBtiles.R
+++ R/predicLA_RF_XGBtiles.R
@@ -2,7 +2,7 @@
 #' @param df the dataframe for building the model
 #' @param rasstack rasterstack, predictors
 #' @param yname the y variable name
-#' @param varstring the variables to subset (grep style)
+#' @param varstring the variables to subset (grep style), by default using all the variables
 #' @param xgbname output filename for xgb
 #' @param rfname output filename for rf
 #' @param lanme output filename for LA
@@ -20,13 +20,13 @@
 #' predicLA_RF_XGBtiles(df, lus, "NO2", xgbname=xgbname, rfname = rfname, laname = laname )}
 
 #' @export
-predicLA_RF_XGBtiles <-function(df, rasstack, yname, varstring = "road_class_|indus", xgbname, rfname, laname, ntree, mtry, gamma, max_depth , eta , nthread = 4, nrounds, ...){
+predicLA_RF_XGBtiles <-function(df, rasstack, yname, varstring=NULL, xgbname, rfname, laname, ntree, mtry, gamma, max_depth , eta , nthread = 4, nrounds, ...){
   predfun <- function(model, data) {
     v <- predict(model, as.matrix(data ))
   }
 
   #indep_dep = subset_grep(df, paste0(yname,"|",varstring) # RESPONSE+PREDICTOR matrix
-  pre_mat3 = subset_grep(df, varstring) # prediction matrix
+  pre_mat3 = ifelse(is.null(varstring), df, subset_grep(df, varstring)) # prediction matrix
   # reorder the dataframe!
   re = names(rasstack)
   pre_mat3 = pre_mat3 %>% dplyr::select (re)
