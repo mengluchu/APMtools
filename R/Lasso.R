715d91d74fa920bfc84ed34a74abcdf44757bc39
Meng Lu <lumeng0312@gmail.com>
2020-04-28 10:55:51 +0200

    remove default values

--- R/Lasso.R
+++ R/Lasso.R
@@ -9,19 +9,19 @@
 #' @export
 
 
-Lasso = function(variabledf, vis1 = T, alpha = 1, y_varname = c("day_value", "night_value", "value_mean"), training, test, grepstring = "ROAD|pop|temp|wind|Rsp|OMI|eleva|coast") {
+Lasso = function(variabledf, vis1 = T, alpha = 1, y_varname, training, test, grepstring ) {
     pre_mat = subset_grep(variabledf, grepstring)
     pre_mat_tr = pre_mat[training, ]
     pre_mat_test = pre_mat[test, ]
     y_tr_value = variabledf[training, y_varname]
     y_test_value = variabledf[test, y_varname]
-    
+
     cvfit <- glmnet::cv.glmnet(as.matrix(pre_mat_tr), y_tr_value, type.measure = "mse", standardize = TRUE, alpha = alpha, lower.limit = 0)
-    
+
     print(cvfit$lambda.min)
     print(cvfit$lambda.1se)
-    
-    
+
+
     if (vis1) {
         plot(cvfit)
         Lassoselected(cvfit)
