eff78fa32a1a532d3406847310f57c0d88d42622
Meng Lu <lumeng0312@gmail.com>
2020-04-28 12:04:49 +0200

    Lasso fixed

--- R/Lasso.R
+++ R/Lasso.R
@@ -11,14 +11,14 @@
 
 Lasso = function(variabledf, vis1 = T, alpha = 1, y_varname, training, test, grepstring ) {
     prenres = paste(y_varname, "|", grepstring, sep = "")
-    pre_mat_all = subset_grep(variabledf[training, ], prenres)
+    pre_mat_all = subset_grep(variabledf, prenres)
     pre_mat = pre_mat_all%>%dplyr::select(-y_varname)
 
     pre_mat_tr = pre_mat[training, ]
     pre_mat_test = pre_mat[test, ]
     y_tr_value = variabledf[training, y_varname]
     y_test_value = variabledf[test, y_varname]
-    print(names(pre_mat_tr))
+
     cvfit <- glmnet::cv.glmnet(as.matrix(pre_mat_tr), y_tr_value, type.measure = "mse", standardize = TRUE, alpha = alpha, lower.limit = 0)
 
     print(cvfit$lambda.min)
--- R/Lasso_pre.R
+++ R/Lasso_pre.R
@@ -3,7 +3,7 @@
 
 Lasso_pre = function(variabledf,  alpha = 1, y_varname = c("day_value", "night_value", "value_mean"), training, test, grepstring = "ROAD|pop|temp|wind|Rsp|OMI|eleva|coast") {
   prenres = paste(y_varname, "|", grepstring, sep = "")
-  pre_mat_all = subset_grep(variabledf[training, ], prenres)
+  pre_mat_all = subset_grep(variabledf, prenres)
   pre_mat = pre_mat_all%>%dplyr::select(-y_varname)
   pre_mat_tr = pre_mat[training, ]
   pre_mat_test = pre_mat[test, ]
