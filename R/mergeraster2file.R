bcc36870e2edc5883d54a8c0078b4b10f64f1042
Meng Lu <lumeng0312@gmail.com>
2020-05-12 14:47:17 +0200

    update rd

--- R/mergeraster2file.R
+++ R/mergeraster2file.R
@@ -1,17 +1,15 @@
 #' Merge new predictor variables (rasters)
 #' @param tomerge the dataframe to merge the raster files to
-#' @param rasterfile the dir of the raster file to merge
+#' @param rasterfile the R raster object (raster package) to merge
 #' @param  varname the name of the new variable
-#' @param lon_lat_name a vector of the names of longitude and latitude.
+#' @param lon_lat_name a vector of the names of longitude and latitude. Commonly Longitude first and latitude second.
 #' @return a dataframe with merged variables
 #' @examples
 #' \donttest{merged = mergeraster2file(merged, c("lon","lat"), 'elevation.map', 'elevation')}
 #' @export
-mergeraster2file = function(tomerge, lon_lat_name,rasterfile, varname) {
-    r = raster(rasterfile)
-
+mergeraster2file = function(tomerge, rasterfile, lon_lat_name, varname) {
     location = tomerge%>%dplyr::select(lon_lat_name)
-    rex = raster::extract(r, location)
+    rex = raster::extract(rasterfile, location)
     merged = cbind(tomerge, rex)
     names(merged)[ncol(merged)] = varname
     merged
