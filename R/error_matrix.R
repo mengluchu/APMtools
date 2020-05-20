e5d781a401d6fef9894788c3901130fecaecce3c
Meng Lu <lumeng0312@gmail.com>
2020-05-20 11:14:50 +0200

    update error matrix

--- R/error_matrix.R
+++ R/error_matrix.R
@@ -1,7 +1,8 @@
 #' error matrix outputs RMSE, MAE, IQR
 #' @param validataion The validataion vector
 #' @param prediction The prediction vector
-#' @return an errormatrix with RMSE, MAE, IQR
+#' @return an errormatrix with RMSE, MAE, IQR, relative RMSE, relative MAE, relative IQR, R-squared, Explained Variance
+#' @details R-squared is caluclated as 1- mse(e)/var(y), which is the same as 1- ss(e)/ss(y). The explained variance is calculated as 1 - var(e)/var(y). For a machine learning technique, the error does not have zero mean. This means the R-squared indicates a reduction in sum of squares of errors (ss), instead of variance. Therefore, an additional indicator, epxlained variance is calculated.
 #' @export
 
     error_matrix = function(validation, prediction) {
@@ -22,8 +23,11 @@
             as.vector(a2[5] - a2[2])/median(test)
             # divided by median
         }
-        Rsquare <- function(test, pred) {
+        explained_variance <- function(test, pred) {
             1-   var(pred - test)/var(test)
+        }
+        Rsquared <- function(test, pred) {
+            1-   mean((pred - test)^2)/var(test)
         }
 
         rrmse <- function(test, pred) {
@@ -41,8 +45,9 @@
         IQR1 = IQR(validation, prediction)
         rIQR1 = rIQR(validation, prediction)
 
-        rsqd1 = Rsquare(validation, prediction)
-        c(RMSE = rmse1, RRMSE = rrmse1, IQR = IQR1, rIQR = rIQR1, MAE=MAE1, rMAE = rMAE1, rsq = rsqd1)
+        rsqd1 = Rsquared(validation, prediction)
+        expvar1 = explained_variance(validation, prediction)
+        c(RMSE = rmse1, RRMSE = rrmse1, IQR = IQR1, rIQR = rIQR1, MAE=MAE1, rMAE = rMAE1, rsq = rsqd1, explained_var = expvar1)
     }
 
 
