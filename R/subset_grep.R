79e11b7d00dc65b1f01e6b5ab2d529f3791519bd
Meng Lu <lumeng0312@gmail.com>
2020-06-04 11:42:34 +0200

    update document

--- R/subset_grep.R
+++ R/subset_grep.R
@@ -1,6 +1,6 @@
 #' subset by grep
 #' @param df the dataframe to subset
-#' @param grepstring the namestring of the colomns to grab, greplstyle
+#' @param grepstring the namestring of the colomns to grab, greplstyle. This function is actually the same as dplyr::select(matches()).
 #' @export
 subset_grep = function(df, grepstring) {
     df[, which(grepl(grepstring, names(df)))]
