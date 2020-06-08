c81c959c3f16af17815abb0403515ad510a835b7
Meng Lu <lumeng0312@gmail.com>
2020-06-08 20:52:45 +0200

    fixed

--- R/prediction_with_pp_La.R
+++ R/prediction_with_pp_La.R
@@ -7,11 +7,11 @@
 
 prediction_with_pp_La = function(rfmodel, trainingXY, trainingY, testingXY)
 {
-  allp = predict(r1,trainingXY , predict.all = T)%>%predictions #get all the tree predictions, instead of the mean
+  allp = predict(rfmodel,trainingXY , predict.all = T)%>%predictions #get all the tree predictions, instead of the mean
   cvfit <- glmnet::cv.glmnet(allp,trainingY ,
                              type.measure = "mse", standardize = TRUE, alpha = 1,
                              lower.limit = 0)
   # aggregate using a regularization, here lasso, you can also do elastic net, training alpha or specify alpha between 0 and 1
-  rpre= predict(r1,testingXY, predict.all=T)%>%predictions # get all the tree predictions
+  rpre= predict(rfmodel,testingXY, predict.all=T)%>%predictions # get all the tree predictions
   predict(cvfit, newx = rpre) # use the regularization (here lasso) model to predict
 }
