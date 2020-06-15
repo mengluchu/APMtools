c20475d4b6dee037307069b4b0b8752cf474f450
Meng Lu <lumeng0312@gmail.com>
2020-06-15 11:02:02 +0200

    update

--- R/ranger_stack.R
+++ R/ranger_stack.R
@@ -8,11 +8,11 @@ ranger_stack = function(df, yname, rasstack, mtry, num.trees )
   formu = as.formula(paste(yname, "~.", sep = ""))
   ra = ranger(formu, df, num.trees = num.trees, mtry = mtry )
 
-  nam = names(sr)
-  sr[is.na(sr[])] = -1
-  names(sr) = nam # fill the missing value to -1
-  rr =predict(ra,  as.matrix(sr))
-  rangerprediction= sr[[1]] # get a layer and then put predictions in
+  nam = names(rasstack)
+  rasstack[is.na(rasstack[])] = -1
+  names(rasstack) = nam # fill the missing value to -1
+  rr =predict(ra,  as.matrix(rasstack))
+  rangerprediction= rasstack[[1]] # get a layer and then put predictions in
   values(rangerprediction)= rr%>%predictions
   rangerprediction
 }
