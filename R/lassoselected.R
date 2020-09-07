d1c26dc3be57ff54312efd9b8ed8c9cb86f5bb0a
Meng Lu <lumeng0312@gmail.com>
2020-09-07 16:03:11 +0200

    return coef

--- R/lassoselected.R
+++ R/lassoselected.R
@@ -8,12 +8,13 @@ Lassoselected = function(cvfit) {
     allvarnames = dimnames(coef(cvfit))[[1]][-1]
     selectedvarnames = allvarnames[coefselected]
     selectedcoef = coefall[coefselected]
-    
+
     ENcoef = selectedcoef
     namesENcoef <- selectedvarnames
-    
+
     ENcoefdf <- data.frame(x = namesENcoef, y = ENcoef, stringsAsFactors = F)
-    
+
     ag = ggplot(ENcoefdf, aes(x = x, y = y)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + labs(title = "coefficient elastic net 0.5", y = "coefficient")
     print(ag)
+    return(namesENcoef)
 }
