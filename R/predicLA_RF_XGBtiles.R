d53d72183a1db3ff78c69d871d2cdffe43b56e40
Meng Lu <lumeng0312@gmail.com>
2021-03-04 11:48:18 +0100

    clean

--- R/predicLA_RF_XGBtiles.R
+++ R/predicLA_RF_XGBtiles.R
@@ -17,10 +17,7 @@
 #' proj = "+proj=longlat +datum=WGS84"
 #' df = retrieve_predictor(lus, bakfile1, c("Lon", "Lat"), proj)
 #' predicLA_RF_XGBtiles(df, lus, "NO2", xgbname=xgbname, rfname = rfname, laname = laname )}
-
 #' @export
-
-
 predicLA_RF_XGBtiles0 = function (df, rasstack, yname, xgbname, rfname, laname, ntree,
                                   mtry, nrounds = 3000, eta = 0.007, gamma = 5, max_depth = 6,
                                   xgb_alpha = 0, xgb_lambda = 2, subsample = 0.7, grepstring)
