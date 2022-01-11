8cda4968037d7e31428f60620c133b7c57780906
Meng Lu <lumeng0312@gmail.com>
2022-01-11 14:24:58 +0100

    fixed crop centre

--- R/crop_center.R
+++ R/crop_center.R
@@ -2,10 +2,10 @@
 
 #'@param ras raster or rasterstack
 #'@param vis if True then apply mapview
-#'@param size windowsize for cropping
+#'@param size windowsize for cropping, number of pixels from the centre.
 #'@export
 
 crop_center= function(ras, size, vis=F,...){
-a = crop(c, extent(c, floor(nrow(c)/2)-size, floor(nrow(c)/2)+size, floor(ncol(c)/2)-size, floor(ncol(c)/2)+size))
+a = crop(c, extent(ras, floor(nrow(ras)/2)-size, floor(nrow(ras)/2)+size, floor(ncol(ras)/2)-size, floor(ncol(ras)/2)+size))
 if(vis)
   mapview(a)}
